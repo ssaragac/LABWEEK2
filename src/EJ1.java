b255af36258980eff28758d0c518aa94c2935e30
ssaragac <grclsara@gmail.com>
2025-06-26 16:01:47 +0200

    Initial commit

--- src/EJ1.java
+++ src/EJ1.java
@@ -1,4 +1,4 @@
-public class MEDIAEDADES {
+public class EJ1 {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
 
---
+++ src/EJ3.java
@@ -0,0 +1,2 @@
+public class EJ3 {
+}
