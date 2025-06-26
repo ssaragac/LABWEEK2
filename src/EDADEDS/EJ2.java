07e63da6b9d6f26a9d5be37088253e6b0357fb1f
ssaragac <grclsara@gmail.com>
2025-06-26 16:55:41 +0200

    Initial commit

--- src/EDADEDS/EJ2.java
+++ src/EDADEDS/EJ2.java
@@ -1,28 +1,10 @@
 package EDADEDS;
-
-import java.util.Scanner;
-
 public class EJ2 {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("¿Cuántos años tienes?:");
-        int age;
-        age = scanner.nextInt();
-
-        if (age < 6 || age > 120) {
-            System.out.println("Edad no válida.");
-        } else if (age >= 6 && age <= 11) {
-            System.out.println("Estás en Primaria.");
-        } else if (age >= 12 && age <=17) {
-            System.out.println("Estás en Secundaria");
-        } else if (age >=18 && age <=23) {
-            System.out.println("Estás en la Universidad.");
-        } else {
-            System.out.println("Estás trabajando.");
+        for (int i = 20; i >=10; i--) {
+            int cuadrado = i * i;
+            System.out.println("El cuadrado de " + i + " es " + cuadrado);
         }
 
-        scanner.close();
     }
 }
