272b32e990d6c07341df8eaf9750ba3241ea6692
ssaragac <grclsara@gmail.com>
2025-06-26 16:35:47 +0200

    Initial commit

--- src/EJ1.java
+++ src/EJ1.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class EJ1 {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¿Cuántos años tienes?:");
+        int age;
+        age = scanner.nextInt();
+
+        if (age < 6 || age > 120) {
+            System.out.println("Edad no válida.");
+        } else if (age >= 6 && age <= 11) {
+            System.out.println("Estás en Primaria.");
+        } else if (age >= 12 && age <=17) {
+            System.out.println("Estás en Secundaria");
+        } else if (age >=18 && age <=23) {
+            System.out.println("Estás en la Universidad.");
+        } else {
+            System.out.println("Estás trabajando.");
+        }
+
+        scanner.close();
     }
 }
